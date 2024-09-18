511ffe0d1acf6ad2f484f80e26827052732f605f
AllanChandler <cph-ac326@cphbusiness.dk>
2024-09-18 10:11:42 +0200

    Update GenreDTO.java

--- src/main/java/org/example/dtos/GenreDTO.java
+++ src/main/java/org/example/dtos/GenreDTO.java
@@ -4,15 +4,31 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.example.entities.Movie;
+import org.example.entities.Genre;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
-@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class GenreDTO {
+    private Long id;
     private String name;
-    private List<Movie> movies;
+    private List<MovieDTO> movies;
+
+    public Genre toEntity() {
+        Genre genre = new Genre();
+        genre.setId(this.id);
+        genre.setName(this.name);
+        return genre;
+    }
+
+    public static GenreDTO fromEntity(Genre genre) {
+        return new GenreDTO(
+                genre.getId(),
+                genre.getName(),
+                genre.getMovies().stream().map(MovieDTO::fromEntity).collect(Collectors.toList())
+        );
+    }
 }
