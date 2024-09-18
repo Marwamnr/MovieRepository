766012456e716aefc5320974556da2ea713a433c
AllanChandler <cph-ac326@cphbusiness.dk>
2024-09-18 10:11:54 +0200

    Update Director.java

--- src/main/java/org/example/entities/Director.java
+++ src/main/java/org/example/entities/Director.java
@@ -1,25 +1,29 @@
 package org.example.entities;
 
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Entity
 @Getter
 @Setter
+@EqualsAndHashCode(of = "id")  // Generer equals() og hashCode() baseret på id for at sikre korrekt funktionalitet
+// i datastrukturer som HashSet og HashMap, hvor objektets identitet er afgørende.
 public class Director {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;  // Instruktørens unikke ID.
 
-    private String name;
+    private String name;  // Instruktørens navn.
 
-    @OneToMany (mappedBy = "director", cascade = CascadeType.ALL)
-    private Set<Movie> movies = new HashSet<>();
+    @OneToMany(mappedBy = "director", cascade = CascadeType.ALL)
+    private Set<Movie> movies = new HashSet<>();  // Liste over film, instruktøren har lavet.
 
+    // Tom konstruktør kræves af JPA for at kunne oprette objekter automatisk.
     public Director() {
     }
 }
