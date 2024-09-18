219b2674f2c949e7a43bcc71a8979122438c1323
AllanChandler <cph-ac326@cphbusiness.dk>
2024-09-18 10:11:46 +0200

    Update MovieDTO.java

--- src/main/java/org/example/dtos/MovieDTO.java
+++ src/main/java/org/example/dtos/MovieDTO.java
@@ -1,25 +1,27 @@
 package org.example.dtos;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-import org.example.entities.Actor;
-import org.example.entities.Genre;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Data
-@ToString
-@AllArgsConstructor
-@NoArgsConstructor
+@Getter
+@Setter
 public class MovieDTO {
+    private boolean adult;
+    private String backdrop_path;
+    private List<Integer> genre_ids;
+    private int id;
+    private String original_language;
+    private String original_title;
+    private String overview;
+    private double popularity;
+    private String poster_path;
+    private String release_date;
     private String title;
-    private int year;
-    private double rating;
-    private List<Genre> genres;
-    private List<Actor> actors;
-    private String director;
-
-
+    private boolean video;
+    private double vote_average;
+    private int vote_count;
 }
