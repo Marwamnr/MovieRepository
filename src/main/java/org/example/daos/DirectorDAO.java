6d5d960b4841c97ced4d1329fc7e911e1670c98a
AllanChandler <cph-ac326@cphbusiness.dk>
2024-09-19 13:48:53 +0200

    Update DirectorDAO.java

--- src/main/java/org/example/daos/DirectorDAO.java
+++ src/main/java/org/example/daos/DirectorDAO.java
@@ -2,45 +2,39 @@ package org.example.daos;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.TypedQuery;
 import org.example.dtos.DirectorDTO;
 import org.example.entities.Director;
 
 import java.util.List;
 
-/*public class DirectorDAO {
+public class DirectorDAO {
 
-    private static DirectorDAO instance;
-    private static EntityManagerFactory emf;
+    private final EntityManagerFactory emf;
 
     public DirectorDAO(EntityManagerFactory emf) {
         this.emf = emf;
     }
 
-    public static DirectorDAO getInstance(EntityManagerFactory emf) {
-        if (instance == null) {
-            instance = new DirectorDAO(emf);
-        }
-        return instance;
-    }
-
     // CREATE
     public DirectorDTO createDirector(DirectorDTO directorDTO) {
-        Director director = directorDTO.toEntity();
         EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
         try {
-            em.getTransaction().begin();
+            tx.begin();
+            Director director = directorDTO.toEntity();
             em.persist(director);
-            em.getTransaction().commit();
+            tx.commit();
+            return DirectorDTO.fromEntity(director);
         } catch (Exception e) {
-            if (em.getTransaction().isActive()) {
-                em.getTransaction().rollback();
+            if (tx.isActive()) {
+                tx.rollback();
             }
             throw e;
         } finally {
             em.close();
         }
-        return DirectorDTO.fromEntity(director);
     }
 
     // READ BY ID
@@ -57,24 +51,20 @@ import java.util.List;
     // UPDATE
     public DirectorDTO updateDirector(DirectorDTO directorDTO) {
         EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
         try {
-            em.getTransaction().begin();
-
+            tx.begin();
             Director director = em.find(Director.class, directorDTO.getId());
             if (director == null) {
                 throw new IllegalArgumentException("Director with ID " + directorDTO.getId() + " not found.");
             }
-
-            // Update basic fields
             director.setName(directorDTO.getName());
-
             em.merge(director);
-            em.getTransaction().commit();
+            tx.commit();
             return DirectorDTO.fromEntity(director);
-
         } catch (Exception e) {
-            if (em.getTransaction().isActive()) {
-                em.getTransaction().rollback();
+            if (tx.isActive()) {
+                tx.rollback();
             }
             throw e;
         } finally {
@@ -85,16 +75,17 @@ import java.util.List;
     // DELETE
     public void deleteDirector(Long id) {
         EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
         try {
-            em.getTransaction().begin();
+            tx.begin();
             Director director = em.find(Director.class, id);
             if (director != null) {
                 em.remove(director);
-                em.getTransaction().commit();
+                tx.commit();
             }
         } catch (Exception e) {
-            if (em.getTransaction().isActive()) {
-                em.getTransaction().rollback();
+            if (tx.isActive()) {
+                tx.rollback();
             }
             throw e;
         } finally {
@@ -106,13 +97,11 @@ import java.util.List;
     public List<DirectorDTO> getAllDirectors() {
         EntityManager em = emf.createEntityManager();
         try {
-            // Use JPQL to select the Director entity
             TypedQuery<Director> query = em.createQuery("SELECT d FROM Director d", Director.class);
             List<Director> directors = query.getResultList();
-            // Convert the list of entities to DTOs
             return directors.stream().map(DirectorDTO::fromEntity).toList();
         } finally {
             em.close();
         }
     }
-}*/
+}
