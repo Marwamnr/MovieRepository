aad5de998b24fbfbebfa6545d6de723d14c2122f
AllanChandler <cph-ac326@cphbusiness.dk>
2024-09-19 13:49:09 +0200

    Update GenreDTO.java

--- src/main/java/org/example/dtos/GenreDTO.java
+++ src/main/java/org/example/dtos/GenreDTO.java
@@ -1,14 +1,31 @@
 package org.example.dtos;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.entities.Genre;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
 public class GenreDTO {
     private Long id;
     private String name;
-}
 
+    public Genre toEntity() {
+        Genre genre = new Genre();
+        genre.setId(this.id);
+        genre.setName(this.name);
+        return genre;
+    }
+
+    public static GenreDTO fromEntity(Genre genre) {
+        if (genre == null) {
+            return null;
+        }
+        return new GenreDTO(
+                genre.getId(),
+                genre.getName()
+        );
+    }
+}
