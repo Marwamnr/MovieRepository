00d72b185a04d5a0d0eaecd6ca77130d6ea31784
AllanChandler <cph-ac326@cphbusiness.dk>
2024-09-20 12:44:14 +0200

    Update Director.java

--- src/main/java/org/example/entities/Director.java
+++ src/main/java/org/example/entities/Director.java
@@ -1,26 +1,23 @@
 package org.example.entities;
 
-import jakarta.persistence.*;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import java.util.HashSet;
-import java.util.Set;
+import jakarta.persistence.*; // Importerer JPA-annotationer
+import lombok.Data; // Importerer Data annotation fra Lombok
+import lombok.EqualsAndHashCode; // Importerer EqualsAndHashCode annotation fra Lombok
+import java.util.HashSet; // Importerer HashSet
+import java.util.Set; // Importerer Set
 
-@Entity
-@Getter
-@Setter
-@EqualsAndHashCode(of = "id")  // Generer equals() og hashCode() baseret på id for at sikre korrekt funktionalitet
-// i datastrukturer som HashSet og HashMap, hvor objektets identitet er afgørende.
+@Entity // Angiver at klassen er en JPA-entitet
+@Data // Genererer getter, setter, toString, equals, og hashCode metoder
+@EqualsAndHashCode(of = "id") // Genererer equals() og hashCode() baseret på id
 public class Director {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id // Angiver ID-feltet som primær nøgle
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Genererer ID automatisk
     private Long id;  // Instruktørens unikke ID.
 
     private String name;  // Instruktørens navn.
 
-    @OneToMany(mappedBy = "director", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "director", cascade = CascadeType.ALL) // En-til-mange relation til Movie
     private Set<Movie> movies = new HashSet<>();  // Liste over film, instruktøren har lavet.
 
     // Tom konstruktør kræves af JPA for at kunne oprette objekter automatisk.
